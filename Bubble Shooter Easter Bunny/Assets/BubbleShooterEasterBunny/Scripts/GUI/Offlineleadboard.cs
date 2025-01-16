6e03efdcf00c565bebcbf1d9c3e89a6e06f863ef
CZY <chenzhengyoung@gmail.com>
2025-01-16 18:23:01 +0800

    1.0

---
+++ Bubble Shooter Easter Bunny/Assets/BubbleShooterEasterBunny/Scripts/GUI/Offlineleadboard.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class Offlineleadboard : MonoBehaviour {
+    TMP_Text label;
+	// Use this for initialization
+	void OnEnable () {
+        label = transform.Find( "Slot" ).Find( "Score" ).GetComponent<TMP_Text>();
+        label.text = "" +  PlayerPrefs.GetInt( "Score" + PlayerPrefs.GetInt( "OpenLevel"));
+	}
+	
+	// Update is called once per frame
+	void OnDisable () {
+        label.text = "";
+	}
+}
